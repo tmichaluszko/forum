46fd18f32340861d945e18b2729721e2390c97b9
Raphael <giribaldiraphael@gmail.com>
2018-11-30 11:53:49 +0100

    <REF>

--- imports/ui/components/list_article/list_article.js
+++ imports/ui/components/list_article/list_article.js
@@ -3,7 +3,6 @@ import { Articles } from "/imports/api/articles/articles.js"
 
 Template.list_article.onCreated(function () {
     this.subscribe("listArt")
-    console.log(Articles.find({}).fetch())
 })
 
 Template.list_article.helpers({
